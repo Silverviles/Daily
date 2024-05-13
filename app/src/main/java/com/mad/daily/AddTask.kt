54b09b2dfc692f095961f1f32db0011b4ca41bc6
Silverviles <sudarakatharindu2001@gmail.com>
2024-05-13 11:22:08 +0000

    Add resource files and priority images

--- app/src/main/java/com/mad/daily/AddTask.kt
+++ app/src/main/java/com/mad/daily/AddTask.kt
@@ -6,13 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    public final val new: Int = 1
-    public final val edit: Int = 2
+class AddTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_task)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/mad/daily/adapter/TaskAdapter.kt
+++ app/src/main/java/com/mad/daily/adapter/TaskAdapter.kt
@@ -1,20 +1,22 @@
 package com.mad.daily.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.mad.daily.AddTask
 import com.mad.daily.MainActivity
 import com.mad.daily.R
 import com.mad.daily.database.AppDatabase
 import com.mad.daily.database.Task
+import java.io.Serializable
 
 class TaskAdapter(
-    private val tasks: List<Task>,
+    private val tasks: MutableList<Task> = mutableListOf(),
     private val layoutId: Int,
     private val mainActivity: MainActivity,
     private val database: AppDatabase
@@ -24,22 +26,45 @@ class TaskAdapter(
         return mainActivity
     }
 
+    fun removeItem(position: Int) {
+        val task = tasks[position]
+        tasks.remove(task)
+        notifyItemRemoved(position)
+        database.taskDao().deleteTask(task)
+    }
+
+    fun editItem(position: Int) {
+        val task = tasks[position]
+        val intent = Intent(mainActivity, AddTask::class.java)
+        intent.putExtra("task", task)
+        intent.putExtra("index", position)
+        mainActivity.startActivity(intent)
+        notifyItemChanged(position)
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
-        var view: View = layoutInflater.inflate(layoutId, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.taskDesc.text = task.taskDesc
 
+        when (task.priority) {
+            0 -> holder.priority.setImageResource(R.drawable.red_circle)
+            1 -> holder.priority.setImageResource(R.drawable.yellow_circle)
+            2 -> holder.priority.setImageResource(R.drawable.green_circle)
+        }
     }
+
 
     override fun getItemCount(): Int {
         return 1
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val taskName: TextView = itemView.findViewById(R.id.tvName)
-        private val priority: ImageView = itemView.findViewById(R.id.ivUrgency)
+        var taskDesc: TextView = itemView.findViewById(R.id.taskDesc)
+        val priority: ImageView = itemView.findViewById(R.id.taskPriority)
     }
 }
